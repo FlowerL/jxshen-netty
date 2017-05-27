ee0f636bd9db7c5809834916a0e37d4e1acc4e1a
沈健翔 <vxlang78@163.com>
2017-05-27 14:46:01 +0800

    fix bug in udp example in netty in action

--- jxshen-netty-server/src/main/java/com/github/jxshen/netty/server/udp/LogEventDecoder.java
+++ jxshen-netty-server/src/main/java/com/github/jxshen/netty/server/udp/LogEventDecoder.java
@@ -8,6 +8,16 @@ import io.netty.channel.socket.DatagramPacket;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.util.CharsetUtil;
 
+/**
+ * reference from: netty in action<br>
+ * </br>
+ * <b>fix bug</b>: in book the logMsg slice start at i+1 and length is buf.readableBytes()<br>
+ * but slice do not influence the readerIndex & writerIndex<br>
+ * so in this code the buf.readableBytes will not change<br>
+ * which leads logMsg.length() to be error<br>
+ * So I fix the slice length to buf.readableBytes() - i - 1<br>
+ * notice the LogEvent.SEPARATOR also take up one position so we need subtract 1 more
+ */
 public class LogEventDecoder extends MessageToMessageDecoder<DatagramPacket> {
 
     @Override
@@ -16,7 +26,8 @@ public class LogEventDecoder extends MessageToMessageDecoder<DatagramPacket> {
         ByteBuf buf = msg.content();
         int i = buf.indexOf(0, buf.readableBytes(), LogEvent.SEPARATOR);
         String filename = buf.slice(0, i).toString(CharsetUtil.UTF_8);
-        String logMsg = buf.slice(i+1, buf.readableBytes()).toString(CharsetUtil.UTF_8);
+        String logMsg = buf.slice(i+1, buf.readableBytes() - i - 1).toString(CharsetUtil.UTF_8);
+        logMsg.replaceAll("/r|/n", "");
         LogEvent event = new LogEvent(msg.sender(), filename, logMsg, System.currentTimeMillis());
         out.add(event);
     }
--- jxshen-netty-server/src/main/java/com/github/jxshen/netty/server/udp/LogEventHandler.java
+++ jxshen-netty-server/src/main/java/com/github/jxshen/netty/server/udp/LogEventHandler.java
@@ -2,13 +2,19 @@ package com.github.jxshen.netty.server.udp;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.util.ReferenceCountUtil;
 
+/**
+ * reference from: netty in action<br>
+ * add function: when meet space char, do not print
+ */
 public class LogEventHandler extends SimpleChannelInboundHandler<LogEvent> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, LogEvent msg) throws Exception {
         StringBuilder builder = new StringBuilder();
+        if (msg.getMsg() == null || msg.getMsg().length() == 0) {
+            return;
+        }
         builder.append(msg.getReceived());
         builder.append("[");
         builder.append(msg.getSource().toString());
